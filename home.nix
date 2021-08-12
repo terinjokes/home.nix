57cf33d25c9284d32e28699a4746ccbfd049ceb3
Terin Stock <terinjokes@gmail.com>
2021-08-11 20:18:40 -0700

    sync refactor

--- home.nix
+++ home.nix
@@ -1,7 +1,9 @@
 { config, pkgs, lib, ... }:
 
-{
-  imports = [ ./modules/xsecurelock ./modules/zsh ];
+let unstable = import <nixpkgs-unstable> { config = config.nixpkgs.config; };
+
+in {
+  imports = [ ./modules/xsecurelock ./modules/zsh ./hosts ];
 
   nixpkgs.config.allowUnfree = true;
   nixpkgs.overlays = [
@@ -11,12 +13,11 @@
         mpdSupport = true;
         pulseSupport = true;
       };
-      google-chrome = super.google-chrome.override {
+      google-chrome = unstable.google-chrome.override {
         commandLineArgs = "--enable-features=WebUIDarkMode --force-dark-mode";
       };
       kubectx = super.callPackage ./packages/kubectx { };
-      aaru =
-        super.callPackage ./packages/aaru { dotnetSDK = super.dotnet-sdk_5; };
+      _1password-gui = unstable._1password-gui;
     })
   ];
 
@@ -31,12 +32,12 @@
   home.packages = with pkgs; [
     dconf
 
+    moreutils
     nixfmt
-    google-chrome
 
     noto-fonts
-    iosevka
-    (iosevka.override { set = "aile"; })
+    iosevka-bin
+    (iosevka-bin.override { variant = "aile"; })
     sarasa-gothic
     emacs-all-the-icons-fonts
 
@@ -44,6 +45,7 @@
     fd
     jq
 
+    git
     gitAndTools.delta
     gitAndTools.ghq
     gitAndTools.stgit
@@ -52,22 +54,11 @@
     # of several other packages.
     (pkgs.callPackage ./packages/openssh-term { })
 
-    pavucontrol
-    herbstluftwm
     xclip
 
-    pinentry_qt5
-    openconnect_openssl
-
     p7zip
-    ark
 
     libsForQt5.qtstyleplugin-kvantum
-    (pkgs.runCommand "qt512-kvantum" { } ''
-      mkdir $out
-      ln -s ${pkgs.libsForQt512.qtstyleplugin-kvantum}/* $out
-      rm $out/bin
-    '')
     (pkgs.runCommand "qt514-kvantum" { } ''
       mkdir $out
       ln -s ${pkgs.libsForQt514.qtstyleplugin-kvantum}/* $out
@@ -77,15 +68,9 @@
     kubectl
     kubectx
 
-    virt-manager
-
-    zoom-us
-
     (aspellWithDicts (dicts: with dicts; [ en en-computers en-science ]))
     hunspell
     hunspellDicts.en-us-large
-
-    aaru
   ];
 
   programs.alacritty = {
@@ -132,335 +117,38 @@
     config = { theme = "Nord"; };
   };
 
-  programs.rofi = {
-    enable = true;
-    theme = "Arc-Dark";
-    extraConfig = ''
-      rofi.show-icons: true
-      rofi.modi: drun,run
-    '';
-  };
-
   programs.fzf = {
     enable = true;
     enableZshIntegration = true;
   };
 
-  xsession = {
-    enable = true;
-    scriptPath = ".hm-xsession";
-    importedVariables = [ "PATH" "QT_STYLE_OVERRIDE" ];
-    windowManager.command = "${pkgs.herbstluftwm}/bin/herbstluftwm";
-
-    numlock.enable = true;
-
-    pointerCursor = {
-      name = "Adwaita";
-      package = pkgs.gnome3.adwaita-icon-theme;
-      size = 16;
-    };
-  };
-
-  services.dunst = {
-    enable = true;
-    settings = {
-      global = {
-        follow = "mouse";
-        geometry = "350x5-20+45";
-        indicate_hidden = "yes";
-        transparency = 0;
-        notification_height = 75;
-        frame_color = "#4C566A";
-        frame_width = 2;
-        separator_color = "frame";
-        separator_height = 5;
-        padding = 10;
-        horizontal_padding = 10;
-        sort = "yes";
-        text = "Iosevka Aile 10";
-        markup = "full";
-        format = "<b>%s</b>\\n%b";
-        word_wrap = "yes";
-        stack_duplicates = true;
-        show_indicators = "yes";
-        icon_position = "left";
-        max_icon_size = 64;
-        dmenu = "${pkgs.rofi}/bin/rofi -dmenu -p dunst";
-        browser = "${pkgs.google-chrome}/bin/google-chrome-stable";
-      };
-      shortcuts = {
-        close = "ctrl+space";
-        close_all = "ctrl+shift+space";
-        history = "ctrl+grave";
-        context = "ctrl+shift+period";
-      };
-      urgency_low = {
-        background = "#3b4252";
-        foreground = "#eceff4";
-        frame_color = "#4c566a";
-        timeout = 10;
-      };
-      urgency_normal = {
-        background = "#3b4252";
-        foreground = "#eceff4";
-        timeout = 10;
-      };
-      urgency_critical = {
-        background = "#3b4252";
-        foreground = "#eceff4";
-        frame_color = "#bf616a";
-        timeout = 0;
-      };
-    };
-  };
-
-  services.polybar = {
-    enable = true;
-    config = {
-      "bar/base" = {
-        width = "100%";
-        height = "22";
-
-        line-size = 1;
-        padding = 0;
-        module-margin = 1;
-
-        font-0 = "Iosevka Aile:size=8;3";
-        font-1 = "Sarasa Mono CL:size=8;3";
-        background = "#2E3440";
-        foreground = "#D8DEE9";
-      };
-      "bar/eDP1" = {
-        "inherit" = "bar/base";
-        monitor = "eDP1";
-
-        modules-left = "xworkspaces";
-        modules-center = "xwindow";
-        modules-right =
-          "tpacpi-kbd_backlight pulseaudio-MGXU pulseaudio-HDAudio battery-BAT0 battery-BAT1 date";
-
-        tray-position = "right";
-      };
-      "bar/DP1" = {
-        "inherit" = "bar/base";
-        monitor = "DP1";
-
-        modules-left = "xworkspaces";
-        modules-center = "xwindow";
-        modules-right =
-          "pulseaudio-MGXU pulseaudio-HDAudio battery-BAT0 battery-BAT1 date";
-
-        tray-position = "right";
-      };
-      "module/battery-BAT0" = {
-        type = "internal/battery";
-        battery = "BAT0";
-        adapter = "AC";
-        format-charging-underline = "#D8DEE9";
-        format-discharging-underline = "#EBCB8B";
-        format-discharging-foreground = "#EBCB8B";
-        label-full-foreground = "#A3BE8C";
-        label-full-underline = "#A3BE8C";
-        label-charging-margin = 1;
-        label-discharging-margin = 1;
-        label-full-margin = 1;
-        label-charging-padding = 1;
-        label-discharging-padding = 1;
-        label-full-padding = 1;
-      };
-      "module/battery-BAT1" = {
-        type = "internal/battery";
-        battery = "BAT1";
-        adapter = "AC";
-        format-charging-underline = "#D8DEE9";
-        format-discharging-underline = "#EBCB8B";
-        format-discharging-foreground = "#EBCB8B";
-        label-full-foreground = "#A3BE8C";
-        label-full-underline = "#A3BE8C";
-        label-charging-margin = 1;
-        label-discharging-margin = 1;
-        label-full-margin = 1;
-        label-charging-padding = 1;
-        label-discharging-padding = 1;
-        label-full-padding = 1;
-      };
-      "module/date" = {
-        type = "internal/date";
-        internal = 5;
-        date = "%Y-%m-%d";
-        time = "%H:%M";
-        label = "%time% %date%";
-        label-margin = 1;
-        label-underline = "#88C0D0";
-        label-padding = 2;
-      };
-      "module/pulseaudio-MGXU" = {
-        type = "internal/pulseaudio";
-        sink = "alsa_output.usb-Yamaha_Corporation_MG-XU-00.iec958-stereo";
-        use-ui-max = false;
-        format-volume-underline = "#B48EAD";
-        label-muted-padding = 1;
-        label-muted-foreground = "#BF616A";
-        label-muted-underline = "#BF616A";
-        label-volume-padding = 1;
-      };
-      "module/pulseaudio-HDAudio" = {
-        type = "internal/pulseaudio";
-        sink = "alsa_output.pci-0000_00_1f.3.analog-stereo";
-        use-ui-max = false;
-        format-volume-underline = "#B48EAD";
-        label-muted-padding = 1;
-        label-muted-foreground = "#BF616A";
-        label-muted-underline = "#BF616A";
-        label-volume-padding = 1;
-      };
-      "module/xworkspaces" = {
-        type = "internal/xworkspaces";
-        format = "<label-state>";
-
-        label-active-underline = "#8FBCBB";
-        label-active-foreground = "#8FBCBB";
-        label-active-padding = 2;
-
-        label-occupied-underline = "#81A1C1";
-        label-occupied-foreground = "#81A1C1";
-        label-occupied-padding = 2;
-
-        label-urgent-foreground = "#BF616A";
-        label-urgent-background = "#D8DEE9";
-        label-urgent-padding = 2;
-
-        label-empty-foreground = "#4c566a";
-        label-empty-padding = 2;
-      };
-      "module/xwindow" = {
-        type = "internal/xwindow";
-        format-foreground = "#8FBCBB";
-        format-underline = "#8FBCBB";
-        format-padding = 1;
-        label = "%title%";
-        label-empty = "-- none --";
-      };
-      "module/tpacpi-kbd_backlight" = {
-        type = "custom/script";
-        tail = true;
-        exec = (pkgs.writeScript "tpacpi-kbd_backlight" ''
-          #!${pkgs.stdenv.shell}
-          path="/sys/devices/platform/thinkpad_acpi/leds/tpacpi::kbd_backlight/brightness"
-
-          status() {
-            case $(<$path) in
-             "0")
-               echo "%{F#4C566A}%{u#4C566A}%{+u}☼%{u-}%{F-}"
-               ;;
-             "1")
-               echo "%{F#5E81AC}%{u#5E81AC}%{+u}☀%{u-}%{F-}"
-               ;;
-             "2")
-               echo "%{F#8FBCBB}%{u#8FBCBB}%{+u}☀%{u-}%{F-}"
-               ;;
-            esac
-          }
-
-          next() {
-            case $(<$path) in
-             "0")
-               echo "1" > $path
-               ;;
-             "1")
-               echo "2" > $path
-               ;;
-             "2")
-               echo "0" > $path
-               ;;
-            esac
-            status
-          }
-
-          trap "status" USR1
-          trap "next" USR2
-
-          while true; do
-            status
-            ${pkgs.coreutils}/bin/sleep 10 &
-            wait $!
-          done
-        '').outPath;
-        click-left = "${pkgs.utillinux}/bin/kill -USR2 %pid%";
-      };
-    };
-    script = "polybar eDP1 & polybar DP1 &";
-  };
-
-  services.picom = {
-    enable = true;
-    vSync = true;
-  };
-
-  services.redshift = {
-    enable = true;
-    provider = "geoclue2";
-  };
-
-  services.xsecurelock.enable = true;
-
-  services.grobi = {
-    enable = true;
-    executeAfter =
-      [ "${pkgs.systemd}/bin/systemctl --user restart polybar.service" ];
-    rules = [
-      {
-        name = "Desk";
-        outputs_connected = [ "DP1" ];
-        outputs_present = [ "eDP1" ];
-        configure_single = "DP1";
-        primary = "DP1";
-        atomic = true;
-        execute_after = [
-          "${pkgs.herbstluftwm}/bin/herbstclient set_monitors 3840x2160+0+0"
-        ];
-      }
-      {
-        name = "Fallback";
-        configure_single = "eDP1";
-        primary = "eDP1";
-        atomic = true;
-        execute_after = [
-          "${pkgs.herbstluftwm}/bin/herbstclient set_monitors 2560x1440+0+0"
-        ];
-      }
-    ];
-  };
-
   programs.direnv = {
     enable = true;
     enableZshIntegration = true;
-    enableNixDirenvIntegration = true;
+    nix-direnv.enable = true;
   };
 
   programs.ssh = {
     enable = true;
     controlMaster = "auto";
     controlPersist = "10m";
+    hashKnownHosts = true;
     extraOptionOverrides = { IgnoreUnknown = "OverrideTerm"; };
     extraConfig = ''
       VisualHostKey yes
     '';
     serverAliveInterval = 60;
     matchBlocks = {
-      "git-bypass-cfaccess.cfdata.org" = {
-        port = 7999;
+      "github.com" = {
         extraOptions = {
           IdentityAgent = "/run/user/1000/yubikey-agent/yubikey-agent.sock";
         };
       };
-      "??m* ?m* ??www* ?www* ??db* ???m* ??com* ???com* ??ndb* ??s* ???s* ??ssds* ??netops* !??dm* !???dm* !?dm* !jump" =
-        {
-          proxyJump = "$(echo %h|grep -o -E '[0-9]+'|head -1).primary.cfops.it";
-          identitiesOnly = true;
-          identityFile = "~/.ssh/id_ed25519";
-          extraOptions = { StrictHostKeyChecking = "accept-new"; };
+      "100.75.69.73" = {
+        extraOptions = {
+          IdentityAgent = "/run/user/1000/yubikey-agent/yubikey-agent.sock";
         };
+      };
     };
   };
 
@@ -472,20 +160,6 @@
       enable = true;
       options = { syntax-theme = "Nord"; };
     };
-    includes = [
-      {
-        path = pkgs.writeText "bitbucket.inc" ''
-          [url "ssh://git@git-bypass-cfaccess.cfdata.org:7999"]
-            insteadOf = ssh://git@stash.cfops.it:7999
-            insteadOf = https://bitbucket.cfdata.org/scm
-            insteadOf = https://bitbucket.cfdata.org
-        '';
-      }
-      {
-        condition = "gitdir:~/cf-repos/";
-        contents = { user.email = "terin@cloudflare.com"; };
-      }
-    ];
     extraConfig = {
       color = {
         diff = "auto";
@@ -495,11 +169,13 @@
       };
       diff.algorithm = "histogram";
       ghq.root = "/home/terin/Development";
-      "ghq \"https://bitbucket.cfdata.org/\"" = {
-        vcs = "git";
-        root = "/home/terin/cf-repos";
-      };
     };
+  };
+
+  programs.gpg = { enable = true; };
+  services.gpg-agent = {
+    enable = true;
+    pinentryFlavor = "qt";
   };
 
   xresources.properties = {
@@ -551,31 +227,10 @@
     Install = { WantedBy = [ "graphical-session.target" ]; };
 
     Service = {
+      Environment = "PATH=${lib.makeBinPath [ pkgs.pinentry-qt ]}";
       ExecStart =
         "${pkgs.yubikey-agent}/bin/yubikey-agent -l %t/yubikey-agent/yubikey-agent.sock";
       ExecReload = "${pkgs.utillinux}/bin/kill -HUP $MAINPID";
-      ProtectSystem = "strict";
-      ProtectKernelLogs = "yes";
-      ProtectKernelModules = "yes";
-      ProtectKernelTunables = "yes";
-      ProtectControlGroups = "yes";
-      ProtectClock = "yes";
-      ProtectHostname = "yes";
-      PrivateTmp = "yes";
-      PrivateDevices = "yes";
-      PrivateUsers = "yes";
-      IPAddressDeny = "any";
-      RestrictAddressFamilies = "AF_UNIX";
-      RestrictNamespaces = "yes";
-      RestrictRealtime = "yes";
-      RestrictSUIDSGID = "yes";
-      LockPersonality = "yes";
-      CapabilityBoundingSet = "";
-      SystemCallFilter = [ "@system-service" "~@privileged @resources" ];
-      SystemCallErrorNumber = "EPERM";
-      SystemCallArchitectures = "native";
-      NoNewPrivileges = "yes";
-      KeyringMode = "private";
       UMask = 177;
       RuntimeDirectory = "yubikey-agent";
     };
@@ -584,16 +239,7 @@
   xdg = {
     enable = true;
     mime.enable = true;
-    mimeApps = {
-      enable = true;
-      defaultApplications = {
-        "text/html" = [ "google-chrome.desktop" ];
-        "x-scheme-handler/http" = [ "google-chrome.desktop" ];
-        "x-scheme-handler/https" = [ "google-chrome.desktop" ];
-        "x-scheme-handler/about" = [ "google-chrome.desktop" ];
-        "x-scheme-handler/unknown" = [ "google-chrome.desktop" ];
-      };
-    };
+    mimeApps.enable = true;
     configFile."Kvantum/kvantum.kvconfig".text = ''
       [General]
       theme=KvArcDark
