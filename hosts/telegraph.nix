b99487d16e07cc2571998fd230baf1320ef169ad
Terin Stock <terinjokes@gmail.com>
2022-07-06 22:38:59 -0700

    polybar: add telegraph modules

--- hosts/telegraph.nix
+++ hosts/telegraph.nix
@@ -161,9 +161,9 @@ in {
         module-margin = 1;
 
         font-0 = "Iosevka Aile:size=8;3";
-        font-1 = "Sarasa Mono CL:size=8";
+        font-1 = "Sarasa Mono CL:size=8;3";
         font-2 = "Noto Emoji:scale=13";
-        font-3 = "FontAwesome:size=8";
+        font-3 = "Material Icons Round:size=10;3";
         background = "#2E3440";
         foreground = "#D8DEE9";
 
@@ -174,8 +174,57 @@ in {
 
         modules-left = "xworkspaces";
         modules-center = "xwindow";
-        modules-right = "info-unread pulseaudio battery date";
+        modules-right = "info-unread pulseaudio battery wireless-network date";
+
         tray-position = "right";
+      };
+      "bar/bottom" = {
+        "inherit" = "bar/base";
+        bottom = true;
+
+        modules-right = "baywheels";
+      };
+      "module/baywheels" = {
+        type = "custom/script";
+        exec = let
+          filter = pkgs.writeTextFile {
+            name = "baywheels-jq-filter";
+            text = ''
+              .data.stations|map(select(.station_id=="930fe54f-5572-4900-8910-6041386560bf"))[0] |
+              {bikes: (.num_bikes_available-.num_ebikes_available), ebikes: .num_ebikes_available} |
+              "%{T4}\ueb29%{T-} \(.bikes) %{T4}\uea0b%{T-} \(.ebikes)"
+            '';
+          };
+        in "${
+          pkgs.writeShellApplication {
+            name = "info-baywheels";
+            runtimeInputs = with pkgs; [ curl jq ];
+            text = ''
+              curl -Ss https://gbfs.baywheels.com/gbfs/en/station_status.json -H"User-Agent: https://terinstock.com" |\
+                jq -r -f ${filter}
+            '';
+          }
+        }/bin/info-baywheels";
+        interval = 60;
+        label-overline = "#88C0D0";
+        label-margin = 1;
+        label-padding = 2;
+      };
+      "module/wireless-network" = {
+        type = "internal/network";
+        interface-type = "wireless";
+        format-connected = "<ramp-signal> <label-connected>";
+        label-connected = "%essid%";
+
+        format-connected-underline = "#88C0D0";
+        format-connected-margin = 1;
+        format-connected-padding = 2;
+
+        ramp-signal-0 = "%{T4}%{T-}";
+        ramp-signal-1 = "%{T4}%{T-}";
+        ramp-signal-2 = "%{T4}%{T-}";
+        ramp-signal-3 = "%{T4}%{T-}";
+        ramp-signal-4 = "%{T4}%{T-}";
       };
       "module/info-unread" = {
         type = "custom/script";
@@ -185,13 +234,15 @@ in {
               runtimeInputs = with pkgs; [ mblaze coreutils ];
               text = ''
                 count=$(mdirs -a ~/Maildir | mlist -s | wc -l)
-                [ "$count" -gt 0 ] && printf '\uf0e0 %s\n' "$count" && exit
+                [ "$count" -gt 0 ] && printf '%%{T4}\uf18a%%{T-} %s\n' "$count" && exit
                 printf '\n'
               '';
             }
           }/bin/info-maildir-unread";
         label-foreground = "#BF616A";
         label-underline = "#BF616A";
+        label-margin = 1;
+        label-padding = 2;
       };
       "module/battery" = {
         type = "internal/battery";
@@ -256,7 +307,10 @@ in {
         label-empty = "-- none --";
       };
     };
-    script = "polybar top &";
+    script = ''
+      polybar top &
+      polybar bottom &
+    '';
   };
 
   systemd.user.services.yubikey-agent = {
